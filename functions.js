675398563789de128819d807f4430c3b414db366
ssani7 <sanaullah.sani756@gmil.com>
2022-01-30 07:51:07 -0800

    first commit

---
+++ functions.js
@@ -0,0 +1,11 @@
+function openSwitch(){
+    console.log('go to the switch');
+    console.log('turn on the switch');
+}
+
+openSwitch();
+
+// properties 
+var item = {
+    
+}
---
+++ java.js
@@ -0,0 +1,35 @@
+var bottleColor = "yellow";
+var waterQuantity = 1;
+var isFull = true;
+
+// array 
+var items = ['bottle', 'mug', 'paper', 'pen'];
+items.indexOf('candy'); //this will be -1
+items.push('envelop');
+items.push('watch');
+items.pop;
+
+// conditions
+if(items.length >= 4){
+    console.log("You have many items on your desk");
+}
+else if(items.length == 4){
+    console.log("You have only 4 itmes");
+}
+else{
+    console.log('you have less than 4 items on your desk');
+}
+
+//loops
+
+// while loops 
+    var i = 0;
+while( i < 10){
+    console.log(i);
+    i++;
+}
+
+// for loops
+for(i = 10; i >= 0; i--){
+    console.log(i);
+}
